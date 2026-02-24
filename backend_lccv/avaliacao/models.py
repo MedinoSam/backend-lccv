93f3400a5ed9f8e4f57d595a78644a088bb7d6c6
samuel <samuelmedino45@gmail.com>
2026-02-24 14:42:49 -0300

    feat: criando enums e subindo no banco

--- backend_lccv/avaliacao/models.py
+++ backend_lccv/avaliacao/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class StatusAvaliacao(models.TextChoices):
+    CRIADA = 'Criada', 'Criada'
+    EM_ELABORACAO = 'Em elaboracao', 'Em elaboracao'
+    EM_AVALIACAO = 'Em avaliacao', 'Em alvaiacao'
+    CONCLUIDA = 'Concluida', 'Concluida'
+
+class DimensaoItemAvaliacao(models.TextChoices):
+    COMPORTAMENTO = 'Comportamento', 'Comportamento'
+    ENTREGAS = 'Entregas', 'Entregas'
+    TRABALHO_EM_EQUIPE = 'Trabalho em equipe', 'Trabalho em equipe'
