ad52110a42321e4dfd5e29592d1b64d2434da3c7
samuel <samuelmedino45@gmail.com>
2026-02-25 21:11:29 -0300

    feat: add whitenoise pra carregar arquivos estaticos

--- backend_lccv/backend_lccv/settings.py
+++ backend_lccv/backend_lccv/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
